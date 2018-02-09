a7fbd234f37aaea126ea60042b78e0eefe8fb8c6
Samira Sánchez <samirasancheze@gmail.com>
2018-02-09 13:19:21 -0400

    Esta es la última versión para la fecha *Recibe bloques de 5 Bytes 	-4 Bytes de informacion y 1Byte de sincronización 	-Grafica lo recibido 	-Muestra en pantalla una pequeña consola con los datos recibidos 		.sensor analogico 1 		.sensor analogico 2 		.sensor digital 1 		.sensor digital 2

---
+++ demoscope3.py
@@ -0,0 +1,69 @@
+import serial
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
+
+i = 0
+t = np.linspace(0, 0.5, 501)
+x = np.zeros(501)
+y = np.zeros(501)
+plt.ion()
+
+ser.isOpen()
+
+while 1:
+	
+	bsync = ser.read(size=1)
+	bsync = int.from_bytes(bsync, sys.byteorder)
+	if bsync == 255:
+		b = ser.read(size=4)
+		b1i = b[0]
+		b2i = b[1]
+		b3i = b[2]
+		b4i = b[3]
+	
+
+		#par1 = b1i >> 7
+		#par2 = b2i >> 7
+		#par3 = b3i >> 7
+		#par4 = b4i >> 7
+	
+
+		d1 = (b1i & 0x20) >> 5
+		d2 = (b3i & 0x20) >> 5
+
+		bi = (((b1i & 0x1F) << 7) + (b2i & 0x7F))/4095
+		bj = (((b3i & 0x1F) << 7) + (b4i & 0x7F))/4095
+
+		x = np.delete(x,0)
+		x = np.append(x,bi)
+	
+		y = np.delete(y,0)
+		y = np.append(y,bj)
+	
+		if i == 0:
+			os.system('clear')
+			print('\nAnag1: '+"%.4f"%bi+' Dig1: '+str(d1)) # +' Dig2: '+str(d2)+' Par1: '+str(par1)+' Par2: '+str(par2))
+			print('Anag2: '+"%.4f"%bj+' Dig2: '+str(d2))#+' Dig4: '+str(d4)+' Par3: '+str(par3)+' Par4: '+str(par4))
+		
+			plt.clf()
+			plt.axis([0, 0.5, 0, 1])
+			plt.plot(t, x, c='b')
+			plt.plot(t, y, c='r')
+			plt.pause(0.00001)
+	
+		i = (i+1)%40
+
+	
+ser.close()
+exit()
